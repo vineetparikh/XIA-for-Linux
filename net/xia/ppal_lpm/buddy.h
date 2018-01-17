669a88b563fb5168a849e0a2b43cd42bf81d45ff
vineetparikh <vince.parikh@gmail.com>
2018-01-17 04:04:09 -0500

    finished code cleanup

--- net/xia/ppal_lpm/buddy.h
+++ net/xia/ppal_lpm/buddy.h
@@ -1,5 +1,5 @@
 /*_
- * Copyright (c) 2014-2015 Hirochika Asai <asai@jar.jp>
+ * Copyright (c) 2014-2015 Hirochika Asai <asai@jar.jp>.
  * All rights reserved.
  */
 
@@ -12,37 +12,33 @@
  * Buddy system
  */
 struct buddy {
-    /* Size of buddy system (# of blocks) */
-    int sz;
-    /* Size of each block */
-    int bsz;
-    /* Bitmap */
-    u8 *b;
-    /* Memory blocks */
-    void *blocks;
-    /* Level */
-    int level;
-    /* Heads */
-    u32 *buddy;
+	/* Size of buddy system (# of blocks). */
+	int sz;
+	/* Size of each block. */
+	int bsz;
+ 	/* Bitmap. */
+	u8 *b;
+	/* Memory blocks. */
+	void *blocks;
+	/* Level. */
+	int level;
+	/* Heads. */
+	u32 *buddy;
 };
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-    /* buddy.c */
-    int buddy_init(struct buddy *, int, int, int);
-    void buddy_release(struct buddy *);
-    void * buddy_alloc(struct buddy *, int);
-    int buddy_alloc2(struct buddy *, int);
-    void buddy_free(struct buddy *, void *);
-    void buddy_free2(struct buddy *, int);
-
+	/* buddy.c. */
+	int buddy_init(struct buddy *, int, int, int);
+	void buddy_release(struct buddy *);
+	void * buddy_alloc(struct buddy *, int);
+	int buddy_alloc2(struct buddy *, int);
+	void buddy_free(struct buddy *, void *);
+	void buddy_free2(struct buddy *, int);
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* _POPTRIE_BUDDY_H */
+#endif /* _POPTRIE_BUDDY_H. */
 
 /*
  * Local variables:
--- net/xia/ppal_lpm/poptrie.h
+++ net/xia/ppal_lpm/poptrie.h
@@ -16,10 +16,10 @@
 
 /* Internal node */
 typedef struct poptrie_node {
-    u64 leafvec;
-    u64 vector;
-    u32 base0;
-    u32 base1;
+	u64 leafvec;
+	u64 vector;
+	u32 base0;
+	u32 base1;
 } poptrie_node_t;
 
 /* Leaf node */
@@ -29,143 +29,143 @@ typedef u16 poptrie_leaf_t;
  * Radix tree node for IPv4
  */
 struct radix_node {
-    int valid;
-    struct radix_node *left;
-    struct radix_node *right;
+	int valid;
+	struct radix_node *left;
+	struct radix_node *right;
 
-    /* Next hop */
-    u32 prefix;
-    int len;
-    poptrie_leaf_t nexthop;
+	/* Next hop */
+	u32 prefix;
+	int len;
+	poptrie_leaf_t nexthop;
 
-    /* Propagated route */
-    struct radix_node *ext;
+	/* Propagated route */
+	struct radix_node *ext;
 
-    /* Mark for update */
-    int mark;
+	/* Mark for update */
+	int mark;
 };
 
 /*
  * Radix tree node for IPv6
  */
 struct radix_node6 {
-    int valid;
-    struct radix_node6 *left;
-    struct radix_node6 *right;
+	int valid;
+	struct radix_node6 *left;
+	struct radix_node6 *right;
 
-    /* Next hop */
-    __uint128_t prefix;
-    int len;
-    poptrie_leaf_t nexthop;
+	/* Next hop */
+	__uint128_t prefix;
+	int len;
+	poptrie_leaf_t nexthop;
 
-    /* Propagated route */
-    struct radix_node6 *ext;
+	/* Propagated route */
+	struct radix_node6 *ext;
 
-    /* Mark for update */
-    int mark;
+	/* Mark for update */
+	int mark;
 };
 
 struct fib_entry {
-    void *nexthop;
-    u32 refcnt;
+	void *nexthop;
+	u32 refcnt;
 };
 
 /*
  * FIB mapping table
  */
 struct poptrie_fib {
-    struct fib_entry *entries;
-    int n;
-    int sz;
+	struct fib_entry *entries;
+	int n;
+	int sz;
 };
 
 /*
  * Poptrie management data structure for IPv4
  */
 struct poptrie {
-    /* Root */
-    u32 root;
+	/* Root */
+	u32 root;
 
-    /* FIB */
-    struct poptrie_fib fib;
+	/* FIB */
+	struct poptrie_fib fib;
 
-    /* Memory management */
-    poptrie_node_t *nodes;
-    poptrie_leaf_t *leaves;
-    void *cnodes;
-    void *cleaves;
+	/* Memory management */
+	poptrie_node_t *nodes;
+	poptrie_leaf_t *leaves;
+	void *cnodes;
+	void *cleaves;
 
-    /* Size */
-    int nodesz;
-    int leafsz;
+	/* Size */
+	int nodesz;
+	int leafsz;
 
-    /* Direct pointing */
-    u32 *dir;
-    u32 *altdir;
+	/* Direct pointing */
+	u32 *dir;
+	u32 *altdir;
 
-    /* RIB */
-    struct radix_node *radix;
+	/* RIB */
+	struct radix_node *radix;
 
-    /* Control */
-    int _allocated;
+	/* Control. */
+	int _allocated;
 };
 
 /*
- * Poptrie management data structure for IPv6
+ * Poptrie management data structure for IPv6.
  */
 struct poptrie6 {
-    /* Root */
-    u32 root;
+	/* Root. */
+	u32 root;
 
-    /* FIB */
-    struct poptrie_fib fib;
+	/* FIB. */
+	struct poptrie_fib fib;
 
-    /* Memory management */
-    poptrie_node_t *nodes;
-    poptrie_leaf_t *leaves;
-    void *cnodes;
-    void *cleaves;
+	/* Memory management. */
+	poptrie_node_t *nodes;
+	poptrie_leaf_t *leaves;
+	void *cnodes;
+	void *cleaves;
 
-    /* Size */
-    int nodesz;
-    int leafsz;
+	/* Size. */
+	int nodesz;
+	int leafsz;
 
-    /* Direct pointing */
-    u32 *dir;
-    u32 *altdir;
+	/* Direct pointing. */
+	u32 *dir;
+	u32 *altdir;
 
-    /* RIB */
-    struct radix_node6 *radix;
+	/* RIB. */
+	struct radix_node6 *radix;
 
-    /* Control */
-    int _allocated;
+	/* Control. */
+	int _allocated;
 };
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-    /* in poptrie.c */
-    struct poptrie * poptrie_init(struct poptrie *, int, int);
-    void poptrie_release(struct poptrie *);
-    int poptrie_route_add(struct poptrie *, u32, int, void *);
-    int poptrie_route_change(struct poptrie *, u32, int, void *);
-    int poptrie_route_update(struct poptrie *, u32, int, void *);
-    int poptrie_route_del(struct poptrie *, u32, int);
-    void * poptrie_exact_lookup(struct poptrie *, u32, u8);
-    void * poptrie_lookup(struct poptrie *, u32);
-    void * poptrie_rib_lookup(struct poptrie *, u32);
-    void * poptrie_rib_lookup_prefix(struct poptrie *, u32, int);
+	/* in poptrie.c */
+	struct poptrie * poptrie_init(struct poptrie *, int, int);
+	void poptrie_release(struct poptrie *);
+	int poptrie_route_add(struct poptrie *, u32, int, void *);
+	int poptrie_route_change(struct poptrie *, u32, int, void *);
+	int poptrie_route_update(struct poptrie *, u32, int, void *);
+	int poptrie_route_del(struct poptrie *, u32, int);
+	void * poptrie_exact_lookup(struct poptrie *, u32, u8);
+	void * poptrie_lookup(struct poptrie *, u32);
+	void * poptrie_rib_lookup(struct poptrie *, u32);
+	void * poptrie_rib_lookup_prefix(struct poptrie *, u32, int);
 
-    /* in poptrie6.c */
-    struct poptrie6 * poptrie6_init(struct poptrie6 *, int, int);
-    void poptrie6_release(struct poptrie6 *);
-    int poptrie6_route_add(struct poptrie6 *, __uint128_t, int, void *);
-    int poptrie6_route_change(struct poptrie6 *, __uint128_t, int, void *);
-    int poptrie6_route_update(struct poptrie6 *, __uint128_t, int, void *);
-    int poptrie6_route_del(struct poptrie6 *, __uint128_t, int);
-    void * poptrie6_lookup(struct poptrie6 *, __uint128_t);
-    void * poptrie6_rib_lookup(struct poptrie6 *, __uint128_t);
+	/* in poptrie6.c */
+	struct poptrie6 * poptrie6_init(struct poptrie6 *, int, int);
+	void poptrie6_release(struct poptrie6 *);
+	int poptrie6_route_add(struct poptrie6 *, __uint128_t, int, void *);
+	int poptrie6_route_change(struct poptrie6 *, __uint128_t, int, void *);
+	int poptrie6_route_update(struct poptrie6 *, __uint128_t, int, void *);
+	int poptrie6_route_del(struct poptrie6 *, __uint128_t, int);
+	void * poptrie6_lookup(struct poptrie6 *, __uint128_t);
+	void * poptrie6_rib_lookup(struct poptrie6 *, __uint128_t);
 
 #ifdef __cplusplus
 }
